5ee3ad92fce40fe33116d87493ecd819b955c987
Jorge Andres Moreno Alvarez <jormoal@SANTASOFIA.LOCAL>
2022-01-26 14:16:55 -0500

    principio del proyecto

---
+++ js/routes.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function() {
+
+    if(XMLHttpRequest){
+        xmlhttp=new XMLHttpRequest()
+    }else{
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP")
+    }
+
+    let compradores=document.getElementById("compradores")
+    compradores.addEventListener("click",function(){
+        
+    })
+
+    let abonos=document.getElementById("abonos")
+    abonos.addEventListener("click",function(){
+        
+    })
+
+    let boletas=document.getElementById("boletas")
+    boletas.addEventListener("click",function(){
+
+    })
+
+    let informes=document.getElementById("informes")
+    informes.addEventListener("click",function(){
+
+    })
+
+})
